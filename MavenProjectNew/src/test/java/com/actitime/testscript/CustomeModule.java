840909b770ec21c18c3018e00241f66f7dedbd31
Vivek <Vivek@DESKTOP-EPG1R7F>
2023-06-05 09:00:06 +0530

    First commit

---
+++ MavenProjectNew/src/test/java/com/actitime/testscript/CustomeModule.java
@@ -0,0 +1,12 @@
+package com.actitime.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CustomeModule {
+
+	@Test
+	public void testCreateCustomer() {
+		Reporter.log("create customer",true);
+	}
+}
